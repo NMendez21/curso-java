3200e8cd2883bfe6db5ffb179d1dabb3e451de14
Nicolás Méndez <mendez.manzanares@gmail.com>
2021-03-15 17:38:18 -0600

    inicio objetos

---
+++ estructuras_principales/src/poo/Uso_Coche.java
@@ -0,0 +1,13 @@
+package poo;
+
+public class Uso_Coche {
+	public static void main(String[] Args)
+	{
+		Coche Renault  = new Coche();
+		
+		System.out.println("Este objeto tiene "+Renault.ruedas+" ruedas.");
+		
+		Coche Peugeot  = new Coche();
+		
+	}
+}
